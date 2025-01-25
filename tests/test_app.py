9003423130d0e6e511af1fb57a9efa773a7d9589
Estevão Siqueira Cunha <estevaosiqueira52@gmail.com>
2025-01-25 15:09:57 -0300

    creating routes and test CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,53 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from todo_fastapi.app import app
-
-
-def test_read_root_return_OK():
-    client = TestClient(app)  # Arrange (organização)
+def test_read_root_return_OK(client):
     response = client.get('/')  # Act (ação)
     assert response.status_code == HTTPStatus.OK  # Assert (verificação)
     assert response.json() == {'message': 'Ola mundo'}
+
+
+def test_create_user_return_201(client):
+    user_data = {
+        'username': 'johndoe',
+        'email': 'teste@gmail.com',
+        'password': 'sdfgsdfgwer',
+    }
+    response = client.post('/users/', json=user_data)
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'username': 'johndoe',
+        'email': 'teste@gmail.com',
+    }
+
+
+def test_read_users_return_list(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'username': 'johndoe',
+                'email': 'teste@gmail.com',
+            }
+        ],
+    }
+
+
+def update_user_return_200(client):
+    user_data = {
+        'password': 'sdfgsdfgwer',
+        'username': 'johndoe',
+        'email': 'test@test.com',
+        'id': 1,
+    }
+
+    response = client.put('/users/1', json=user_data)
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'johndoe',
+        'email': 'test@test.com',
+    }
--- todo_fastapi/app.py
+++ todo_fastapi/app.py
@@ -1,8 +1,35 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
+from .schemas import UserDB, UserList, UserPublic, UserSchema
+
 app = FastAPI()
+
+database = []
 
 
 @app.get('/')
 def read_root():
     return {'message': 'Ola mundo'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get('/users/', response_model=UserList)
+def return_users():
+    return {'users': database}
+
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    user_with_id = UserDB(id=user_id, **user.model_dump())
+    database[user_id - 1] = user_with_id
+    return user_with_id
