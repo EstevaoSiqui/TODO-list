daf96ccd36ab4f09f73fb0019d29cfb2691a2e47
Estevão Siqueira Cunha <estevaosiqueira52@gmail.com>
2025-02-13 21:17:15 -0300

    Adding more test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,19 @@
 import pytest
 from fastapi.testclient import TestClient
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from models import table_registry
 from todo_fastapi.app import app
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine)
+
+
+    table_registry.metadata.drop_all(engine)
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,10 +1,25 @@
-from models import User
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import Session
+
+from models import User, table_registry
 
 
-def test_create_user():
-    user = User(
-        username='johndoe',
-        email='teste@email.com',
-        password='123456',
-    )
-    assert user.id is None
+def test_create_user_db():
+    engine = create_engine('sqlite:///:memory:')
+
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        user = User(
+            username='johndoe',
+            email='teste@email.com',
+            password='123456',
+        )
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+
+        result = session.scalar(
+            select(User).where(User.email == 'teste@email.com')
+        )
+    assert result.username == 'johndoe'
